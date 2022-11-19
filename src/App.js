7ce941770acb033e657a76ebc0fd2b393161dfa9
batjona24 <tahirajbatjona@gmail.com>
2022-11-19 22:20:17 +0100

    box-generator assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react';
+import Display from "./components/Display"
+import Form from "./components/Form"
+import Ninja from "./components/Ninja"
+import FormNinja from "./components/FormNinja" 
+
 
 function App() {
+  const [boxColorArray, setBoxColorArray] = useState([]);
+  const [ ninjaBoxArray, setNinjaBoxArray ] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Standard Assignment</h1>
+      <Form boxColorArray={ boxColorArray } setBoxColorArray={ setBoxColorArray } />
+      <Display boxColorArray={ boxColorArray } />
+      <hr />
+      <h1>Ninja Assignment</h1>
+      <FormNinja ninjaBoxArray={ ninjaBoxArray } setNinjaBoxArray={ setNinjaBoxArray } />
+      <Ninja ninjaBoxArray={ ninjaBoxArray } />
     </div>
   );
 }
